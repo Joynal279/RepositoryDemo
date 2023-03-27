bce13bca574f5ef7d20d0f8dcb02d6bb2c899ca1
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-28 05:18:18 +0600

    initial commit

---
+++ RepositoryDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RepositoryDemo.Models;
+
+namespace RepositoryDemo.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+	}
+}
+
---
+++ RepositoryDemo/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepositoryDemo.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Please enter name")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Please enter email")]
+		[EmailAddress(ErrorMessage ="Email should be valid")]
+		public string Email { get; set; }
+		[Required(ErrorMessage ="Please enter reg no.")]
+		public string RegNo { get; set; }
+
+	}
+}
+
